bfbd0abeedc2fae52b0ccf52c9342fe1b103cc1e
DarkneszzFN <90118039+ImnotMaikol@users.noreply.github.com>
2021-10-13 16:41:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	int d1,d2, suma =0,resta =0;
+
+	
+	cout<<"Digite un numero: "; cin>>d1;
+	cout<<"Digite otro numero: "; cin>>d2;
+	
+	suma = d1 + d2;
+	resta = d1 - d2;
+cout<<"\nLa suma es: "<<suma<<endl;
+	cout<<"La resta es: "<<resta<<endl;
+  	return 0;
+}
